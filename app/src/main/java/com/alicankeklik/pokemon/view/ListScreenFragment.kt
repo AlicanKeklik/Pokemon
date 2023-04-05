cebfee577ce7e76549b43c33f34d74184349fbc9
Alican Keklik <alicaniron10@gmail.com>
2023-04-05 21:25:18 +0200

    description[  overlayscreen over other apps]

--- app/src/main/java/com/alicankeklik/pokemon/view/ListScreenFragment.kt
+++ app/src/main/java/com/alicankeklik/pokemon/view/ListScreenFragment.kt
@@ -2,23 +2,18 @@ package com.alicankeklik.pokemon.view
 
 
 import android.content.Context
-import android.content.Intent
 import android.graphics.PixelFormat
-import android.os.Build
 import android.os.Bundle
-import android.provider.Settings
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
 import android.widget.Toast
-import androidx.core.content.ContextCompat.startForegroundService
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.alicankeklik.pokemon.R
 import com.alicankeklik.pokemon.databinding.FragmentListScreenBinding
-import com.alicankeklik.pokemon.databinding.OverlayScreenBinding
 import com.alicankeklik.pokemon.viewmodel.PokemonViewModel
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.overlay_screen.view.*
@@ -27,6 +22,11 @@ import kotlinx.android.synthetic.main.overlay_screen.view.*
 class ListScreenFragment : Fragment() {
    private lateinit var  binding: FragmentListScreenBinding
    private lateinit var viewModel: PokemonViewModel
+   private lateinit var testView : View
+   private lateinit var wm : WindowManager
+   private lateinit var mParams : WindowManager.LayoutParams
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,7 +37,17 @@ class ListScreenFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
+
+        testView = LayoutInflater.from(context).inflate(R.layout.overlay_screen, null)
+         mParams = WindowManager.LayoutParams(
+            400,
+            400,
+            WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
+            WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+            PixelFormat.TRANSLUCENT)
+        wm = context?.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+         // Inflate the layout for this fragment
         binding = FragmentListScreenBinding.inflate(inflater, container, false)
         val view = binding.root
         return view
@@ -52,35 +62,39 @@ class ListScreenFragment : Fragment() {
         observeLiveData()
         viewModel.loadData(ApiUrl)
 
-       binding.button.setOnClickListener{
+        binding.button.setOnClickListener{
           // overlaycall
            startService()
        }
 
     }
     fun startService() {
-        val testView = LayoutInflater.from(context).inflate(R.layout.overlay_screen, null)
-        viewModel.pokemons.observe(viewLifecycleOwner,{
-            testView.overlayname.text = it.pokemonDetailName
-            Picasso.with(context).load(it.pokemonDetailImgUrl?.url.toString()).resize(400,400).centerInside().into(testView.viewFront)
-            Picasso.with(context).load(it.pokemonDetailImgUrl?.urlBack.toString()).resize(400,400).centerInside().into(testView.viewBack)
 
-        })
-        val mParams: WindowManager.LayoutParams? = WindowManager.LayoutParams(
-            400,
-            400,
-            WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
-            WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN,
-            PixelFormat.TRANSLUCENT)
-
-
-        val wm = context?.getSystemService(Context.WINDOW_SERVICE) as WindowManager
         wm.addView(testView, mParams)
 
-        testView.closeoverlay.setOnClickListener{
-            wm.removeView(testView)
-        }
+            viewModel.pokemons.observe(viewLifecycleOwner, {
+                testView.overlayname.text = it.pokemonDetailName
+                Picasso.with(context).load(it.pokemonDetailImgUrl?.url.toString()).resize(400, 400)
+                    .centerInside().into(testView.viewFront)
+                Picasso.with(context).load(it.pokemonDetailImgUrl?.urlBack.toString())
+                    .resize(400, 400).centerInside().into(testView.viewBack)
+            })
 
+//         Log.e("ListScreenFragment" ,"isOverlayScreenWorking2" +isOverlayCreated)
+//            viewModel.pokemons.observe(viewLifecycleOwner,{
+//                testView.overlayname.text = it.pokemonDetailName
+//                Picasso.with(context).load(it.pokemonDetailImgUrl?.url.toString()).resize(400,400).centerInside().into(testView.viewFront)
+//                Picasso.with(context).load(it.pokemonDetailImgUrl?.urlBack.toString()).resize(400,400).centerInside().into(testView.viewBack)
+//
+//            })
+
+
+
+        testView?.let {
+            it.closeoverlay.setOnClickListener{
+             wm.removeView(testView)
+
+        } }
 
     }
 
@@ -91,7 +105,6 @@ class ListScreenFragment : Fragment() {
                 binding.textView2.text = it.pokemonDetailHeight
                 binding.textView3.text = it.pokemonDetailWeight
                 Picasso.with(context).load(it.pokemonDetailImgUrl?.url.toString()).resize(400,400).centerInside().into(binding.imageView)
-                Log.e("Alican","pokemon name :"+it.pokemonDetailName.toString())
             }
 
 
@@ -106,6 +119,5 @@ class ListScreenFragment : Fragment() {
 
         })
     }
-
 
 }
